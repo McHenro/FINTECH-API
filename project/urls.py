de7710e0d6314e4840c10261a1351289d929166a
Henry <adolphosita@yahoo.com>
2022-12-14 09:57:11 +0100

    .

--- project/urls.py
+++ project/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api_docs/', schema_view),
     path('', include('speedapp.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
 ]
 
